51b77d6a08482d0e0253fa1e1857bd5c4e57f5b1
Brett Rivers <riverswb@gmail.com>
2018-03-20 22:01:03 -0600

    CRUD functionality for trucks is complete

---
+++ NoCoFoodTrucks/Migrations/20180321034424_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace NoCoFoodTrucks.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Truck",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    Facebook = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Twitter = table.Column<string>(nullable: true),
+                    Website = table.Column<string>(nullable: true),
+                    Yelp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Truck", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Truck");
+        }
+    }
+}
