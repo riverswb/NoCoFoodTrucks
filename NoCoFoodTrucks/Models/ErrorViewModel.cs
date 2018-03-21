4a3ac476e02ab26065156561a592e6febac38f36
Brett Rivers <riverswb@gmail.com>
2018-03-20 21:22:15 -0600

    Add project files.

---
+++ NoCoFoodTrucks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NoCoFoodTrucks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
